b9f592d39beeb43883a4513340534a2fca445d3e
Roman Maximov <manul99991@mail.ru>
2022-09-10 16:29:28 +0300

    HomeWork

---
+++ HomeWork2/task_5.py
@@ -0,0 +1,9 @@
+#Реализуйте алгоритм перемешивания списка.
+import random
+n = int(input('Введите количество элументов списка: '))
+ls = []
+for i in range(n):
+    ls.append(random.randrange(-n, n)) #random.randrange(start, stop, step) - возвращает случайно выбранное число из последовательности
+print(ls)
+random.shuffle(ls) #random.shuffle(sequence, [rand]) - перемешивает последовательность (изменяется сама последовательность).
+print(f'Перемешанный список: {ls}')
