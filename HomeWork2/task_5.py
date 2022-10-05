d3775811f544182b874ac0808769118846e69645
Roman Maximov <manul99991@mail.ru>
2022-10-05 14:33:28 +0300

    HomeWork

--- HomeWork2/task_5.py
+++ HomeWork2/task_5.py
@@ -2,8 +2,14 @@
 import random
 n = int(input('Введите количество элументов списка: '))
 ls = []
-for i in range(n):
-    ls.append(random.randrange(-n, n)) #random.randrange(start, stop, step) - возвращает случайно выбранное число из последовательности
+# for i in range(n):
+#     ls.append(random.randrange(-n, n)) #random.randrange(start, stop, step) - возвращает случайно выбранное число из последовательности
+# print(ls)
+# random.shuffle(ls) #random.shuffle(sequence, [rand]) - перемешивает последовательность (изменяется сама последовательность).
+# print(f'Перемешанный список: {ls}')
+
+
+[ls.append(random.randrange(-n, n)) for i in range(n)]
 print(ls)
-random.shuffle(ls) #random.shuffle(sequence, [rand]) - перемешивает последовательность (изменяется сама последовательность).
-print(f'Перемешанный список: {ls}')
+random.shuffle(ls)
+print(ls)
---
+++ HomeWork6/task1.py
@@ -0,0 +1,18 @@
+# Задайте последовательность чисел. Напишите программу, которая выведет список
+# неповторяющихся элементов исходной последовательности.
+# [1, 1, 2, 3, 4, 5, 5] -> [2, 3, 4]
+my_list = [1, 1, 2, 3, 4, 5, 5]
+unique_list = []
+
+for i in my_list:
+    count = 0
+    for j in my_list:
+        if i == j:
+            count +=1
+    if count == 1:
+        unique_list.append(i)
+print(f'Дан список: {my_list}\nНеповторяющиеся элементы списка: {unique_list}')
+
+#Улучшенное решение
+my_list = list(filter(lambda x: my_list.count(x) < 2 in my_list, my_list))
+print(f'Дан список: {my_list}\nНеповторяющиеся элементы списка: {unique_list}')
